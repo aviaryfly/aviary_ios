63c1e81d3febaf88edf0a063990b974df40bad62
Xinyu Fang <f813779259@gmail.com>
2026-04-27 22:02:56 -0400

    Initial Commit

---
+++ Aviary/AviaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  AviaryApp.swift
+//  Aviary
+//
+//  Created by Xinyu Fang on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct AviaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
